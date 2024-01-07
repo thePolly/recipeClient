19a0125b9ee46c4f96b831ed652a543fc3d79ed4
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2024-01-07 21:32:19 +0100

    added a get request

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
 
